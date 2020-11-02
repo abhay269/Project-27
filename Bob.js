7cd28ca6caa874519bcf15fb2c30566dc22450b7
abhay269 <70260307+abhay269@users.noreply.github.com>
2020-11-02 15:43:45 +0530

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -1,27 +1,33 @@
 class Bob {
     constructor(x,y,r){
+        
         var options={
             isStatic:false,
             friction:0.4,
             density:3,
            restitution:1.2,
+            
         }
         this.x=x;
         this.y=y;
-        this.r=r;
+        this.radius = radius;
 
-        this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+        this.body=Bodies.circle(this.x,this.y,this.radius/2,options)
         World.add(world,this.body);
+        
     }
+    
     display(){
+        
         var pos = this.body.position;
-        push ()
+        push ();
         translate(pos.x,pos.y);
         rotate (this.body.angle)
         fill ("pink");
 
         ellipseMode(CENTER);
-        ellipse(0,0,this.r,this.r)
-        pop ()
+        ellipse(0,0,this.radius,this.radius)
+        pop ();
+        
     }
 }
